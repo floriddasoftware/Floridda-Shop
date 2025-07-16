df7aaf24750fc2a9d0b67a7d57a70df79facf3a1
Floridda Software <floriddasoftware@gmail.com>
2025-07-16 01:27:52 +0100

    f

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,4 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono } from "next/font/google"
 import "./globals.css";
 import BackToTop from "@/components/BackToTop";
 import { ToastContainer } from "react-toastify";
